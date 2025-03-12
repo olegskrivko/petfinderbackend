941117b1dfc620bd16906165d43006dcc05d702b
Olegs <olegs.krivko@gmail.com>
2025-03-12 02:33:49 +0200

    add

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from datetime import timedelta
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
